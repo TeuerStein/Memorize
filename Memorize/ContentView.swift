7227193372ca9c059e6b5f4cd3c664ec2487bdbd
TeuerStein <xenkai123456789@gmail.com>
2020-10-23 20:28:20 +0200

    Was created a mechanism for building 4 objects

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }
             .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👨‍💻")
+                .padding()
+        }
     }
 }
 
